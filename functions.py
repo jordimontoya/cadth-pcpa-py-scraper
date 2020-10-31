d943a76f9ff77918c360847e5f14cfe02d67e761
Jordi Montoya <jmontoya@stacks-cis.ct>
2020-10-31 19:21:04 +0100

    parsing products

--- functions.py
+++ functions.py
@@ -1,7 +1,68 @@
 import requests
 from bs4 import BeautifulSoup
-import csv
+import codecs
+from config import *
 
 def scrapBaseUrl(url):
     r = requests.get(url)
     return BeautifulSoup(r.text, 'lxml')
+
+# Returns excel columns' head as a string
+def getExcelHead(table, arr_head):
+    thead = [e.text for e in table.find("thead").find_all("th")]
+    return SEPARATOR.join(thead) + '|url|' + SEPARATOR.join(arr_head) + '\n'
+
+# Returns excel row as a string
+def getExcelRow_1(tr):
+    table_row = SEPARATOR.join( [e.get_text(separator=" ").strip() for e in tr.find_all("td")] )
+    print(table_row + '\n')
+
+    url_product = BASE_URL_1 + '' + tr.td.a['href']
+    soup = scrapBaseUrl("https://www.cadth.ca/pembrolizumab-keytruda-hnscc-details")
+    product_detail = getProductDetail_1(soup)
+
+    return table_row + '|' + url_product + '|' + product_detail + '\n'
+
+# Returns the detail row as a string
+def getProductDetail_1(soup):
+    row = ''
+
+    #1st detected format (ex: https://www.cadth.ca/xalkori-resubmission-first-line-advanced-nsclc-details)
+    #2nd detected format (ex: https://www.cadth.ca/ibrutinib-imbruvica-leukemia)
+    if soup.find("table", class_=TABLE_PRODUCT_CLASS_1):
+
+        product_tr_list = soup.find("table", class_=TABLE_PRODUCT_CLASS_1)
+        product_row = []
+
+        for element in THEAD_PRODUCT_1:
+            if product_tr_list.find("th", string=element):
+                string = product_tr_list.find("th", string=element).find_next_sibling("td").get_text(separator=" ").strip()
+                string = string.replace('\n', ' ').replace('\r', '')
+                product_row.append(string)
+            else:
+                product_row.append("")
+
+        row = SEPARATOR.join(product_row)
+
+    #3rd detected format (ex: https://www.cadth.ca/aripiprazole-25)    
+    elif soup.find("div", class_="publish-date"):
+        for element in THEAD_PRODUCT_1:
+            
+            if element == "Manufacturer":
+                #clean manufacturer value
+                manufacturer = soup.find("p", class_="field_manufacturer")
+                manufacturer.strong.decompose()
+                row = row + manufacturer.get_text(separator=" ").strip()
+            
+            elif element == "Submission Type" and soup.find("p", class_="field_submission_type"):
+                #clean submission type value
+                submission_type = soup.find("p", class_="field_submission_type")
+                submission_type.strong.decompose()
+                row = row + submission_type.get_text(separator=" ").strip()
+            else:
+                row = row + "|"
+
+    else:
+        row = "Unable to fetch data, new web format"
+
+    return row
--- run.py
+++ run.py
@@ -1,102 +1,21 @@
-from config import *
 from functions import *
+from config import *
 
+# Scraps table from BASE_URL_1
 soup = scrapBaseUrl(BASE_URL_1 + PATH_1)
-
 table = soup.find("table", class_=TABLE_CLASS_1)
-thead = [e.text for e in table.find("thead").find_all("th")]
-trs = table.find_all("tr")
 
-#opens csv file
-f = open('csvfile.csv','w')
+# Opens csv file
+f = codecs.open(OUTPUT_FILE_1, "w", "utf-8")
 
-#builds excel head
-excel_head = SEPARATOR.join(thead) + '|url' + THEAD_PRODUCT_1 + '\n'
-
-#builds excel data
-#excel_data = ''
-excel_row = ''
-for tr in trs:
-    
-    excel_row = SEPARATOR.join( [e.text for e in tr.find_all("td")] )
-    
-    url_product = BASE_URL_1 + '' + tr.td.a['href']
-    
-    soup_product = scrapBaseUrl(url_product)
-
-    row = ''
-    #1st detected format (ex: https://www.cadth.ca/xalkori-resubmission-first-line-advanced-nsclc-details)
-    if soup_product.find("table", class_=TABLE_PRODUCT_CLASS_1):
-        product_table = soup_product.find("table", class_=TABLE_PRODUCT_CLASS_1)
-        
-        remove_list = ["Brand Name","Generic Name","Indication","Review Status","Final Recommendation Issued","Submission Date"]
-
-        product_row = []
-        for product_tr in product_table.find_all("tr"):
-            if product_tr.find("th").text in remove_list:
-                product_row.append("")
-            else:
-                product_row.append(product_tr.find("td").text)
-
-        row = SEPARATOR.join(product_row)
-
-    #2nd detected format (ex: https://www.cadth.ca/aripiprazole-25)    
-    elif soup_product.find("div", class_="publish-date"):
-        project_number = soup_product.find("div", class_="publish-project-number").find("span").text
-        strength = ""
-        tumour_type = ""
-        funding_request = ""
-        pre_noc_submission = ""
-        noc_date = ""
-        
-        #clean manufacturer value
-        manufacturer_name = soup_product.find("p", class_="field_manufacturer")
-        manufacturer_name.strong.decompose()
-        manufacturer_name = manufacturer_name.text
-        
-        sponsor = ""
-        submission_deemed_completed = ""
-        
-        #clean submission type value value
-        submission_type = soup_product.find("p", class_="field_submission_type")
-        submission_type.strong.decompose()
-        submission_type = submission_type.text
-        
-        prioritization_request = ""
-        stakeholder_input_deadline = ""
-        checkpoint_meeting = ""
-        perc_meeting = ""
-        initial_recommendation_issued = ""
-        feedback_deadline = ""
-        perc_reconsideration = ""
-        notification_to_implement = ""
-        clarification = ""
-
-        row = project_number + '|' + strength + '|' + tumour_type + '|' + funding_request + '|' + pre_noc_submission + '|' + noc_date + '|' + manufacturer_name + '|' + sponsor + '|' + submission_deemed_completed + '|' + submission_type + '|' + prioritization_request + '|' + stakeholder_input_deadline + '|' + checkpoint_meeting + '|' + perc_meeting + '|' + initial_recommendation_issued + '|' + feedback_deadline + '|' + perc_reconsideration + '|' + notification_to_implement + '|' + clarification
-
-    else:
-        row = "Unable to fetch data, new web format"
-
-    excel_row = excel_row + '|' + url_product + '|' + row + '\n'
-
-    #print("ROW : " + excel_row + '\n')
-    
-#excel_data = excel_data + excel_row
-
-f = open('csvfile.csv','w')
+# Builds and writes excel's head
+excel_head = getExcelHead(table, THEAD_PRODUCT_1)
 f.write(excel_head)
-f.write(excel_row)
+
+# Builds and writes excel's data
+for tr in table.find_all("tr"):
+    excel_row = getExcelRow_1(tr)
+    f.write(excel_row)
+
+# Close csv file
 f.close()
-
-#csvOpen = open('filename','w')
-#c = csv.writer(csvOpen, dialect='excel')
-#c.writerows(excel_data)
-
-#f.write(th.string)
-#f.write(th.string'hi there\n') # Python will convert \n to os.linesep
-
-#for tr in trs:
-    #for td in tr:
-        #print(td.string)
-
-#f.close()
